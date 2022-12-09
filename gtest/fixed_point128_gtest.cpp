37291f5824cdab4754cf6e662740955c5d49fc19
ericgur <33372943+ericgur@users.noreply.github.com>
2022-12-09 18:30:22 +0200

    More unit tests

--- gtest/fixed_point128_gtest.cpp
+++ gtest/fixed_point128_gtest.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <ostream>
 //#include <gmock/gmock.h>
 #include "..\inc\fixed_point128.h"
 
@@ -14,17 +15,56 @@ using namespace fp128;
 
 
 // Construct fixed_point128 from various elements.
-TEST(FixedPoint128Construction, FromDouble) {
-    double doubleValues[] = { 0.0, 1.0, 2.5, 0.001, 255.000001 };
-
-    for (auto i = 0; i < array_length(doubleValues); ++i) {
-        double dval = doubleValues[i];
-        fixed_point128<10> f = dval;
-        EXPECT_DOUBLE_EQ(static_cast<double>(f), dval);
+TEST(FixedPoint128Construction, Double) {
+    double values[] = { 0.0, 1.0, 2.5, 0.001, 255.000001 };
+    for (auto i = 0u; i < array_length(values); ++i) {
+        fixed_point128<10> f = values[i];
+        EXPECT_DOUBLE_EQ(static_cast<double>(f), values[i]);
     }
-    
-    // Expect two strings not to be equal.
-    //EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    //EXPECT_EQ(7 * 6, 42);
+}
+TEST(FixedPoint128Construction, Float) {
+    float values[] = { 0.0f, 1.0f, 2.5f, 0.001f, 255.000001f };
+    for (auto i = 0u; i < array_length(values); ++i) {
+        fixed_point128<10> f = values[i];
+        EXPECT_FLOAT_EQ(static_cast<float>(f), values[i]);
+    }
+}
+TEST(FixedPoint128Construction, Int32) {
+    int32_t values[] = { 0, 10, -1543 };
+    for (auto i = 0u; i < array_length(values); ++i) {
+        fixed_point128<20> f = values[i];
+        EXPECT_EQ(static_cast<int32_t>(f), values[i]);
+    }
+}
+TEST(FixedPoint128Construction, UnsignedInt32) {
+    uint32_t values[] = { 0u, 10u, 1543u };
+    for (auto i = 0u; i < array_length(values); ++i) {
+        fixed_point128<20> f = values[i];
+        EXPECT_EQ(static_cast<uint32_t>(f), values[i]);
+    }
+}
+TEST(FixedPoint128Construction, Int64) {
+    uint64_t values[] = { 0ll, 10ll, 1543ll };
+    for (auto i = 0u; i < array_length(values); ++i) {
+        fixed_point128<20> f = values[i];
+        EXPECT_EQ(static_cast<int64_t>(f), values[i]);
+    }
+}
+
+TEST(FixedPoint128Construction, UnsignedInt64) {
+    uint64_t values[] = { 0ull, 10ull, 1543ull };
+    for (auto i = 0u; i < array_length(values); ++i) {
+        fixed_point128<20> f = values[i];
+        EXPECT_EQ(static_cast<uint64_t>(f), values[i]);
+    }
+}
+
+TEST(FixedPoint128Construction, String) {
+    const char* values[] = { "0", "12.34"};
+    for (auto i = 0u; i < array_length(values); ++i) {
+        fixed_point128<20> f = values[i];
+        double d1 = strtod(values[i], nullptr);
+        double d2 = strtod(static_cast<char*>(f), nullptr);
+        EXPECT_DOUBLE_EQ(d1, d2);
+    }
 }
--- gtest/test.bat
+++ gtest/test.bat
@@ -1,7 +1,8 @@
 @echo off
+cls
 set cur_dir=%CD%
 cd build
 
-ctest
+ctest --output-on-failure
 
 cd %cur_dir%
