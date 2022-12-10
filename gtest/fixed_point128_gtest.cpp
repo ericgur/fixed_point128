b8fd02fcee7f649ce32224e9c0c03a461fdf5f7a
ericgur <33372943+ericgur@users.noreply.github.com>
2022-12-10 15:03:36 +0200

    Cosmetics

--- gtest/fixed_point128_gtest.cpp
+++ gtest/fixed_point128_gtest.cpp
@@ -14,52 +14,75 @@ using namespace fp128;
 //using ::testing::DoubleEq;
 
 
-// Construct fixed_point128 from various elements.
-TEST(FixedPoint128Construction, Double) {
-    double values[] = { 0.0, 1.0, 2.5, 0.001, 255.000001 };
+// Construct fixed_point128 and convert back to/from various elements.
+TEST(fixed_point128, ConstructorFromDouble) {
+    double values[] = { 0.0, 1.0, 2.5, 0.001, 255.000001, 1e38 };
     for (auto i = 0u; i < array_length(values); ++i) {
-        fixed_point128<10> f = values[i];
+        fixed_point128<20> f = values[i];
+        if (values[i] > f.max_int_value) {
+            printf("Warning: test used a value which is too high: %lf\n", values[i]);
+            continue;
+        }
+
         EXPECT_DOUBLE_EQ(static_cast<double>(f), values[i]);
     }
 }
-TEST(FixedPoint128Construction, Float) {
+TEST(fixed_point128, ConstructorFromFloat) {
     float values[] = { 0.0f, 1.0f, 2.5f, 0.001f, 255.000001f };
     for (auto i = 0u; i < array_length(values); ++i) {
-        fixed_point128<10> f = values[i];
+        fixed_point128<20> f = values[i];
+        if (values[i] > f.max_int_value) {
+            printf("Warning: test used a value which is too high: %f\n", values[i]);
+            continue;
+        }
         EXPECT_FLOAT_EQ(static_cast<float>(f), values[i]);
     }
 }
-TEST(FixedPoint128Construction, Int32) {
+TEST(fixed_point128, ConstructorFromInt32) {
     int32_t values[] = { 0, 10, -1543 };
     for (auto i = 0u; i < array_length(values); ++i) {
         fixed_point128<20> f = values[i];
+        if (abs(values[i]) > f.max_int_value) {
+            printf("Warning: test used a value which is too high: %d\n", values[i]);
+            continue;
+        }
         EXPECT_EQ(static_cast<int32_t>(f), values[i]);
     }
 }
-TEST(FixedPoint128Construction, UnsignedInt32) {
+TEST(fixed_point128, ConstructorFromUnsignedInt32) {
     uint32_t values[] = { 0u, 10u, 1543u };
     for (auto i = 0u; i < array_length(values); ++i) {
         fixed_point128<20> f = values[i];
+        if (values[i] > f.max_int_value) {
+            printf("Warning: test used a value which is too high: %u\n", values[i]);
+            continue;
+        }
         EXPECT_EQ(static_cast<uint32_t>(f), values[i]);
     }
 }
-TEST(FixedPoint128Construction, Int64) {
-    uint64_t values[] = { 0ll, 10ll, 1543ll };
+TEST(fixed_point128, ConstructorFromInt64) {
+    int64_t values[] = { 0ll, 10ll, 1543ll };
     for (auto i = 0u; i < array_length(values); ++i) {
         fixed_point128<20> f = values[i];
+        if (abs(values[i]) > f.max_int_value) {
+            printf("Warning: test used a value which is too high: %lld\n", values[i]);
+            continue;
+        }
         EXPECT_EQ(static_cast<int64_t>(f), values[i]);
     }
 }
-
-TEST(FixedPoint128Construction, UnsignedInt64) {
+TEST(fixed_point128, ConstructorFromUnsignedInt64) {
     uint64_t values[] = { 0ull, 10ull, 1543ull };
     for (auto i = 0u; i < array_length(values); ++i) {
         fixed_point128<20> f = values[i];
+        if (values[i] > f.max_int_value) {
+            printf("Warning: test used a value which is too high: %llu\n", values[i]);
+            continue;
+        }
         EXPECT_EQ(static_cast<uint64_t>(f), values[i]);
     }
 }
-
-TEST(FixedPoint128Construction, String) {
+TEST(fixed_point128, ConstructorFromString) {
     const char* values[] = { "0", "12.34"};
     for (auto i = 0u; i < array_length(values); ++i) {
         fixed_point128<20> f = values[i];
